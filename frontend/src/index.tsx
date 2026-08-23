1d63a607229fd6141c575e8a94024a47f633fdc7
Millene Yoshino Russo <milleneyoshinorusso@outlook.com>
2026-08-23 11:10:05 -0300

    chamar vendedor

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { App } from './App'
+import { CallSellerButton } from './components/CallSellerButton'
 
 const root = createRoot(document.getElementById('root')!)
 root.render(
@@ -11,6 +12,7 @@ root.render(
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
+      <CallSellerButton />
     </BrowserRouter>
   </React.StrictMode>,
 )
